232a437024320b9e966715df693ccdef8766079b
philip <g30613740@gmail.com>
2026-05-27 11:52:14 +0300

    upgrade C++ code: renamed variables, added constants, comments

--- include/functions.h
+++ include/functions.h
@@ -4,34 +4,38 @@
 
 using namespace std;
 
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
 int resources_filling (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z,
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights,
     const size_t len_XYZ,
     const long double bound_a,
     const long double bound_b);
 
 long double get_polynoms_value_in_the_point (
-    long double *a_x,
-    long double *a_y,
-    long double *a_z,
+    long double *x_nodes,
+    long double *y_nodes,
+    long double *weights,
     const size_t len_XYZ,
     const long double point);
 
 void freeing_up_resources (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z);
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights);
 
 long double get_value_of_the_math_function_in_the_point (long double point);
 
 void extract_parameters_to_the_file (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z,
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights,
     const size_t len_XYZ,
     const size_t len_of_grid,
     const long double bound_a,
     const long double bound_b);
+
+#endif
--- src/functions.cpp
+++ src/functions.cpp
@@ -1,16 +1,17 @@
 #include "functions.h"
 
+const long double MIN_DISTANCE = 1.0e-3;
 
 int resources_filling (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z,
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights,
     const size_t len_XYZ,
     const long double bound_a,
     const long double bound_b) {
 
-    a_x = new long double[len_XYZ];                          // array of x_i
     // uniform distribution filling array x
+    x_nodes = new long double[len_XYZ];                      // array of nodes of Lagrange polynom
     random_device rd;                                        // a seed source for the random number engine
     mt19937 gen (rd ());                                     // mersenne_twister_engine seeded with rd()
     uniform_real_distribution<> distrib (bound_a, bound_b);  // segment boundaries
@@ -18,14 +19,14 @@ int resources_filling (
     while (true) {
         if (k == len_XYZ - 1)
             break;
-        a_x[k] = distrib (gen);
+        x_nodes[k] = distrib (gen);
         if (k == 1) {
             ++k;
             continue;
         }
         unsigned int count = 0;
         for (size_t i = 1; i < k; ++i) {
-            if (fabs (a_x[k] - a_x[i]) < 1.0e-3)
+            if (fabs (x_nodes[k] - x_nodes[i]) < MIN_DISTANCE)
                 ++count;
         }
         if (count > 0)
@@ -34,37 +35,33 @@ int resources_filling (
             ++k;
     }
   
-    a_x[0] = bound_a;
-    a_x[len_XYZ-1] = bound_b;
+    x_nodes[0] = bound_a;
+    x_nodes[len_XYZ-1] = bound_b;
 
     // filling array y
-    a_y = new long double[len_XYZ];
+    y_nodes = new long double[len_XYZ];
     for (size_t i = 0; i < len_XYZ; ++i)
-        a_y[i] = get_value_of_the_math_function_in_the_point (a_x[i]);
+        y_nodes[i] = get_value_of_the_math_function_in_the_point (x_nodes[i]);
 
-    // filling an array z
-    // z is the array of denumenator values
-    a_z = new long double[len_XYZ];
+    // w is the array of denomenator values
+    weights = new long double[len_XYZ];
     for (size_t i = 0; i < len_XYZ; ++i) {
         long double prod = 1.;
         for (size_t j = 0; j < len_XYZ; ++j) {
             if (i == j)
                 continue;
-            prod *= a_x[i] - a_x[j];
+            prod *= x_nodes[i] - x_nodes[j];
         }
-        a_z[i] = prod;
+        weights[i] = prod;
     }
-
-    if (a_x == nullptr or a_y == nullptr or a_z == nullptr)
-    	return -1;
 
     return 0;
 }
 
 long double get_polynoms_value_in_the_point (
-    long double* a_x,
-    long double* a_y,
-    long double* a_z,
+    long double* x_nodes,
+    long double* y_nodes,
+    long double* weights,
     const size_t len_XYZ,
     const long double point) {
 
@@ -73,21 +70,21 @@ long double get_polynoms_value_in_the_point (
         long double prod = 1.; // numerator
         for (size_t j = 0; j < len_XYZ; ++j) {
             if (i != j)
-                prod *= point - a_x[j];
+                prod *= point - x_nodes[j];
         }
-        sum += a_y[i] * prod / a_z[i]; 
+        sum += y_nodes[i] * prod / weights[i]; 
     }
     return sum;
 }
 
 void freeing_up_resources (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z) {
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights) {
 
-    delete [] a_x;
-    delete [] a_y;
-    delete [] a_z;
+    delete [] x_nodes;
+    delete [] y_nodes;
+    delete [] weights;
 }
 
 long double get_value_of_the_math_function_in_the_point (long double point) {
@@ -95,9 +92,9 @@ long double get_value_of_the_math_function_in_the_point (long double point) {
 }
 
 void extract_parameters_to_the_file (
-    long double* &a_x,
-    long double* &a_y,
-    long double* &a_z,
+    long double* &x_nodes,
+    long double* &y_nodes,
+    long double* &weights,
     const size_t len_XYZ,
     const size_t len_of_grid,
     const long double bound_a,
@@ -109,12 +106,12 @@ void extract_parameters_to_the_file (
     file << bound_a << '\n' << bound_b << '\n' << len_XYZ << '\n' << len_of_grid << '\n';    // 1st string: a and b and M
 
     for (size_t i = 0; i < len_XYZ; ++i) {
-        file << a_x[i] << ' ';
+        file << x_nodes[i] << ' ';
     }
     file << '\n';
 
     for (size_t i = 0; i < len_XYZ; ++i) {
-        file << a_y[i] << ' ';
+        file << y_nodes[i] << ' ';
     }
     file << '\n';
 
@@ -129,8 +126,8 @@ void extract_parameters_to_the_file (
     file << get_value_of_the_math_function_in_the_point (bound_b) << endl;
 
     for (size_t i = 0; i < len_of_grid - 1; ++i)                                             // 4th string: values of lagrange polynom in points of grid
-        file << get_polynoms_value_in_the_point (a_x, a_y, a_z, len_XYZ, bound_a + delta * i) << ' ';
-    file << get_polynoms_value_in_the_point (a_x, a_y, a_z, len_XYZ, bound_b) << endl;
+        file << get_polynoms_value_in_the_point (x_nodes, y_nodes, weights, len_XYZ, bound_a + delta * i) << ' ';
+    file << get_polynoms_value_in_the_point (x_nodes, y_nodes, weights, len_XYZ, bound_b) << endl;
 
     file.close ();
 }
